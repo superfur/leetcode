6fb38826ca3025d8b7c53474e514ad21d334cf7c
admin <superfur@gmail.com>
2025-07-22 15:43:46 +0800

    feat: update 0722

---
+++ solutions/java/0020.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            }
+            else if (stack.isEmpty() || map.get(stack.pop()) != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
