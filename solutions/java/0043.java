ce2942a20a6add543d1d69d68e213705e2f50f93
Septem-pc <superfur@163.com>
2025-11-08 13:49:03 +0800

    feat: update 1108

---
+++ solutions/java/0043.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if ("0".equals(num1) || "0".equals(num2)) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            int digit1 = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int digit2 = num2.charAt(j) - '0';
+                int mul = digit1 * digit2;
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                int sum = mul + result[p2];
+
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean leadingZero = true;
+        for (int value : result) {
+            if (leadingZero && value == 0) {
+                continue;
+            }
+            leadingZero = false;
+            sb.append(value);
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
