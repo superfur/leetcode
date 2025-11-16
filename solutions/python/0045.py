9ac0fb8feec52dec84990bf3eac21b34545e3815
admin <superfur@gmail.com>
2025-11-16 11:40:02 +0800

    feat: update 1116-2

--- solutions/python/0045.py
+++ solutions/python/0045.py
@@ -1,29 +1,3 @@
-# function jump(nums: number[]): number {
-#     const n = nums.length;
-#     if (n <= 1) {
-#         return 0;
-#     }
-
-#     let jumps = 0;
-#     let currentEnd = 0;
-#     let farthest = 0;
-
-#     for (let i = 0; i < n - 1; i++) {
-#         farthest = Math.max(farthest, i + nums[i]);
-
-#         if (i === currentEnd) {
-#             jumps++;
-#             currentEnd = farthest;
-
-#             if (currentEnd >= n - 1) {
-#                 break;
-#             }
-#         }
-#     }
-
-#     return jumps;
-# };
-
 from typing import List
 
 class Solution:
