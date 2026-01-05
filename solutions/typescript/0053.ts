b130d86c18b23efa3c5d31d2cad3c07897f26a4a
Septem-pc <superfur@163.com>
2026-01-05 09:49:59 +0800

    feat: update 0105

---
+++ solutions/typescript/0053.ts
@@ -0,0 +1,51 @@
+/**
+ * maxSubArray 返回数组中连续子数组的最大和
+ * 
+ * 时间复杂度: O(n) - 只需遍历数组一次
+ * 空间复杂度: O(1) - 只使用了常数额外空间
+ * 
+ * 思路: 使用 Kadane 算法（动态规划/贪心思想）
+ * 1. 遍历数组，维护两个变量：
+ *    - currentSum: 以当前元素结尾的最大子数组和
+ *    - maxSum: 全局最大子数组和
+ * 2. 对于每个元素：
+ *    - 如果 currentSum < 0，说明之前的子数组是负贡献，应该舍弃
+ *      从当前元素重新开始（currentSum = nums[i]）
+ *    - 否则，将当前元素加入子数组（currentSum += nums[i]）
+ *    - 更新全局最大值 maxSum = max(maxSum, currentSum)
+ * 
+ * 核心思想: 当累加和变为负数时，说明这部分对后续没有贡献，
+ *           应该丢弃，从下一个元素重新开始计算
+ * 
+ * @param nums 整数数组
+ * @returns 最大子数组和
+ * 
+ * @example
+ * maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) // 返回 6 (子数组 [4,-1,2,1])
+ */
+function maxSubArray(nums: number[]): number {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  let maxSum = nums[0];
+  let currentSum = nums[0];
+
+  // 从第二个元素开始遍历
+  for (let i = 1; i < nums.length; i++) {
+    // 如果当前累加和为负，舍弃之前的部分，从当前元素重新开始
+    if (currentSum < 0) {
+      currentSum = nums[i];
+    } else {
+      // 否则继续累加当前元素
+      currentSum += nums[i];
+    }
+
+    // 更新全局最大值
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+    }
+  }
+
+  return maxSum;
+}
