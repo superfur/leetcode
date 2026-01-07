910c7d696585df4e0a33d1e0c7849788649334a2
admin <superfur@gmail.com>
2026-01-07 22:31:44 +0800

    feat: update 0107

---
+++ solutions/rust/0054.rs
@@ -0,0 +1,94 @@
+impl Solution {
+    /*
+     * 螺旋矩阵 - 边界模拟法
+     * 
+     * 时间复杂度: O(m * n)，其中 m 是行数，n 是列数
+     * 空间复杂度: O(1)，除了结果数组外只使用常数个变量
+     * 
+     * 思路:
+     * 1. 定义四个边界：top, bottom, left, right
+     * 2. 按照 右->下->左->上 的顺序遍历
+     * 3. 每完成一个方向，更新对应的边界
+     * 4. 当 top > bottom 或 left > right 时停止
+     * 
+     * 遍历顺序:
+     * - 向右：从 left 到 right，遍历 top 行
+     * - 向下：从 top+1 到 bottom，遍历 right 列
+     * - 向左：从 right-1 到 left，遍历 bottom 行（逆序）
+     * - 向上：从 bottom-1 到 top+1，遍历 left 列（逆序）
+     * 
+     * 示例:
+     * matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     * 第一轮：
+     *   - 向右：[1,2,3]
+     *   - 向下：[6,9]
+     *   - 向左：[8,7]
+     *   - 向上：[4]
+     * 第二轮：
+     *   - 向右：[5]
+     * 输出：[1,2,3,6,9,8,7,4,5]
+     */
+    pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
+        if matrix.is_empty() || matrix[0].is_empty() {
+            return Vec::new();
+        }
+        
+        let m = matrix.len();
+        let n = matrix[0].len();
+        let mut result = Vec::with_capacity(m * n);
+        
+        // 定义四个边界
+        let mut top = 0;
+        let mut bottom = m - 1;
+        let mut left = 0;
+        let mut right = n - 1;
+        
+        while top <= bottom && left <= right {
+            // 向右：遍历 top 行，从 left  to right
+            for j in left..=right {
+                result.push(matrix[top][j]);
+            }
+            top += 1;
+            
+            // 向下：遍历 right 列，从 top 到 bottom
+            // 需要检查是否还有列可以遍历
+            if top > bottom {
+                break;
+            }
+            for i in top..=bottom {
+                result.push(matrix[i][right]);
+            }
+            if let Some(new_right) = right.checked_sub(1) {
+                right = new_right;
+            } else {
+                break;
+            }
+            
+            // 向左：遍历 bottom 行，从 right 到 left（逆序）
+            // 需要检查是否还有行可以遍历，并且 right >= left
+            if top > bottom || left > right {
+                break;
+            }
+            for j in (left..=right).rev() {
+                result.push(matrix[bottom][j]);
+            }
+            if let Some(new_bottom) = bottom.checked_sub(1) {
+                bottom = new_bottom;
+            } else {
+                break;
+            }
+            
+            // 向上：遍历 left 列，从 bottom 到 top（逆序）
+            // 需要检查是否还有列可以遍历，并且 bottom >= top
+            if left > right || top > bottom {
+                break;
+            }
+            for i in (top..=bottom).rev() {
+                result.push(matrix[i][left]);
+            }
+            left += 1;
+        }
+        
+        result
+    }
+}
