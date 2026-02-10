dee73d004f6b843bfee4bb31a2c6ed99d0bacf1a
admin <superfur@gmail.com>
2026-02-10 21:45:11 +0800

    feat: udpate 2010

--- solutions/typescript/0059.ts
+++ solutions/typescript/0059.ts
@@ -1,9 +1,40 @@
-/**
- * 螺旋矩阵 II
- */
-function 螺旋矩阵Ii(...args: any[]): any {
-    // TODO: 实现你的解法
-    return null;
+function generateMatrix(n: number): number[][] {
+    const matrix: number[][] = Array.from({ length: n }, () => Array(n).fill(0));
+
+    let top = 0;
+    let bottom = n - 1;
+    let left = 0;
+    let right = n - 1;
+    let num = 1;
+    const max = n * n;
+
+    while (num <= max) {
+        // 向右：填充 top 行
+        for (let j = left; j <= right && num <= max; j++) {
+            matrix[top][j] = num++;
+        }
+        top++;
+
+        // 向下：填充 right 列
+        for (let i = top; i <= bottom && num <= max; i++) {
+            matrix[i][right] = num++;
+        }
+        right--;
+
+        // 向左：填充 bottom 行
+        for (let j = right; j >= left && num <= max; j--) {
+            matrix[bottom][j] = num++;
+        }
+        bottom--;
+
+        // 向上：填充 left 列
+        for (let i = bottom; i >= top && num <= max; i--) {
+            matrix[i][left] = num++;
+        }
+        left++;
+    }
+
+    return matrix;
 }
 
-export default 螺旋矩阵Ii;
+export default generateMatrix;
